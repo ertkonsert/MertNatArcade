af9c4660c8dffc2b72cc7da7f3a29e4100ba3cd7
nataliekrus <60926545+nataliekrus@users.noreply.github.com>
2020-02-11 13:51:05 +0100

    Add files via upload

---
+++ supersida/index.js
@@ -0,0 +1,13 @@
+const memoryBox = document.getElementById("box1");
+memoryBox.addEventListener("click", openMemory);
+
+function openMemory() {
+    window.location.href = "memory.html";
+}
+
+const quizBox = document.getElementById("box2");
+quizBox = document.getElementById("click", openQuiz);
+
+function openQuiz () {
+    // window.location.href = 
+}
